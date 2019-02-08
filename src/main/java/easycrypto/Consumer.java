e33b822d85237c25e74bd9907af86d4ac0964f38
Victor Monte <victorbcm@hotmail.com>
2019-02-08 17:38:15 -0200

    Add consumer interface

--- src/main/java/easycrypto/Consumer.java
+++ src/main/java/easycrypto/Consumer.java
@@ -1,4 +1,22 @@
 package easycrypto;
 
-public class Consumer {
+import java.util.Properties;
+
+public interface Consumer {
+
+    static Properties createConfig(String servers, String groupId) {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", servers);
+        properties.put("group.id", groupId);
+        properties.put("enable.auto.commit", "true");
+        properties.put("auto.commit.interval.ms", "1000");
+        properties.put("auto.offset.reset", "earliest");
+        properties.put("session.timeout.ms", "30000");
+        properties.put("key.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        return properties;
+    }
+
 }
