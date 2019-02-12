2211f08181c6eb92996729f35a50c59bc18107c4
Victor Monte <victorbcm@hotmail.com>
2019-02-12 16:19:39 -0200

    Add enricher producer

---
+++ src/main/java/easycrypto/core/Enricher.java
@@ -0,0 +1,58 @@
+package easycrypto.core;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.maxmind.geoip2.model.CityResponse;
+import easycrypto.infraestructure.Producer;
+import easycrypto.service.GeoIPService;
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+import java.io.IOException;
+
+public class Enricher implements Producer {
+
+    private final KafkaProducer<String, String> producer;
+    private final String validMessages;
+    private final String invalidMessages;
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    public Enricher(String servers, String validMessages, String invalidMessages) {
+        this.producer = new KafkaProducer<>(Producer.createConfig(servers));
+        this.validMessages = validMessages;
+        this.invalidMessages = invalidMessages;
+    }
+
+    @Override
+    public void process(String message) {
+
+        try {
+
+            final JsonNode root = MAPPER.readTree(message);
+            final JsonNode ipAddressNode = root.path("customer").path("ipAddress");
+
+            if(ipAddressNode.isMissingNode()) {
+
+                Producer.write(
+                        this.producer,
+                        this.invalidMessages,
+                        "{\"error\": \"customer.ipAddress is missing\"}");
+            } else {
+
+                final String ipAddress = ipAddressNode.textValue();
+                final CityResponse response = new GeoIPService().getLocation(ipAddress);
+
+                ((ObjectNode) root).with("customer").put("country", response.getCountry().getName());
+                ((ObjectNode) root).with("customer").put("city", response.getCity().getName());
+
+                Producer.write(
+                        this.producer,
+                        this.validMessages,
+                        MAPPER.writeValueAsString(root));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
