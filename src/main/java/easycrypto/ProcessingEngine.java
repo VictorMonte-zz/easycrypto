63f09352613bbc03ac243d683b9d61b22f9d0d3b
Victor Monte <victorbcm@hotmail.com>
2019-02-12 09:48:16 -0200

    Add main class

---
+++ src/main/java/easycrypto/ProcessingEngine.java
@@ -0,0 +1,18 @@
+package easycrypto;
+
+public class ProcessingEngine {
+
+    public static void main(String[] args) {
+
+        String servers = args[0];
+        String groupId = args[1];
+        String sourceTopic = args[2];
+        String targetTopic = args[3];
+
+        Reader reader = new Reader(servers, groupId, sourceTopic);
+        Writer writer = new Writer(servers, targetTopic);
+
+        reader.run(writer);
+
+    }
+}
