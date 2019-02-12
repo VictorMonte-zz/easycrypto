04904da4b04de8528efdb8d05c6c274064d01eaf
Victor Monte <victorbcm@hotmail.com>
2019-02-12 09:49:23 -0200

    Log reader

--- src/main/java/easycrypto/Reader.java
+++ src/main/java/easycrypto/Reader.java
@@ -3,12 +3,15 @@ package easycrypto;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.time.Duration;
 import java.util.Collections;
 
 public class Reader implements Consumer {
 
+    private static final Logger logger = LogManager.getLogger();
     private final KafkaConsumer<String, String> consumer;
     private final String topic;
 
@@ -17,12 +20,13 @@ public class Reader implements Consumer {
         this.topic = topic;
     }
 
-    @SuppressWarnings("InfiniteLoopStatement")
     public void run(Producer producer) {
+        logger.info("Subscribing to topic " + topic);
         this.consumer.subscribe(Collections.singletonList(topic));
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
             for (ConsumerRecord<String, String> record : records) {
+                logger.info("Record: " + record.value());
                 producer.process(record.value());
             }
         }
