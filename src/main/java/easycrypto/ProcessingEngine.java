ed14569ffa8657c4f7c61ad2b4574ba56847e93b
Victor Monte <victorbcm@hotmail.com>
2019-02-21 08:32:02 -0300

    Add app in docker compose

--- src/main/java/easycrypto/ProcessingEngine.java
+++ src/main/java/easycrypto/ProcessingEngine.java
@@ -2,17 +2,16 @@ package easycrypto;
 
 import easycrypto.core.Enricher;
 import easycrypto.core.Reader;
-import easycrypto.core.Validator;
 
 public class ProcessingEngine {
 
     public static void main(String[] args) {
 
-        String servers = args[0];
-        String groupId = args[1];
-        String sourceTopic = args[2];
-        String validTopic = args[3];
-        String invalidTopic = args[4];
+        String servers = System.getenv("SERVERS");
+        String groupId = System.getenv("GROUP_ID");
+        String sourceTopic = System.getenv("SOURCE_TOPIC");
+        String validTopic = System.getenv("VALID_TOPIC");
+        String invalidTopic = System.getenv("INVALID_TOPIC");
 
         Reader reader = new Reader(servers, groupId, sourceTopic);
         Enricher enricher = new Enricher(servers, validTopic, invalidTopic);
